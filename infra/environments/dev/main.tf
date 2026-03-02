7c25477b5e8480f37d373ac0e0b1b0351c4c1bdb
Luka <nemamnik16@gmail.com>
2026-03-02 18:14:55 +0000

    Phase 2 Day 5 - Add IaC security scanning

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -2,7 +2,20 @@ module "logs_bucket" {
   source   = "../../../modules/storage"
   name     = "logs-dev-${random_id.suffix.hex}"
   location = "EU"
+  log_bucket = module.access_logs_bucket.bucket_name
 }
+
+module "access_logs_bucket" {
+  source   = "../../../modules/storage"
+  name     = "access-logs-dev-${random_id.suffix.hex}"
+  location = "EU"
+  log_bucket = null
+}
+
+
+
+
+
 
 resource "random_id" "suffix" {
   byte_length = 2
